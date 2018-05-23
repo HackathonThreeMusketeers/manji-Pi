7da9d7cecd92a50a37c03544acd2403055425b0f
hiriorima <g2117001@fun.ac.jp>
2018-05-23 07:23:13 +0000

    fix #1: modify using library to pyaudio

--- recognition.py
+++ recognition.py
@@ -1,36 +1,52 @@
-import fileinput
-import os
-import re
-import subprocess
-import sys
-import tempfile
-import textwrap
-import traceback
-import aiy.audio
-from aiy._drivers._hat import get_aiy_device_name
+import pyaudio
+import time
+import wave
+import numpy
 import speech_recognition
 
-RECORD_DURATION_SECONDS = 3
 
 def check_mic_works():
-    """Check the microphone records correctly."""
-    temp_file, temp_path = tempfile.mkstemp(suffix='.wav')
-    os.close(temp_file)
 
+    CHUNK = 1024
+    FORMAT = pyaudio.paInt16
+    CHANNELS = 2
+    RATE = 44100
+    RECORD_SECONDS = 3
+    WAVE_OUTPUT_FILENAME = "output.wav"
     result = ""
 
-    try:
-        print('Recording...')
-        aiy.audio.record_to_wave(temp_path, RECORD_DURATION_SECONDS)
-        
-        r = speech_recognition.Recognizer()
-        with speech_recognition.AudioFile(temp_path) as source:
-                audio = r.record(source)
-                result = r.recognize_google(audio, language='ja-JP')
-    finally:
-        try:
-            os.unlink(temp_path)
-        except FileNotFoundError:
-            pass
+    input_device_index = 0
+    p = pyaudio.PyAudio()
+    stream = p.open(format=FORMAT,
+            channels=CHANNELS,
+            rate=RATE,
+            input=True,
+            frames_per_buffer=CHUNK)
+
+    print("recording")
+
+    frames = []
+
+    for i in range(0, int(RATE / CHUNK * RECORD_SECONDS)):
+        data = stream.read(CHUNK)
+        frames.append(data)
+
+    print("finish")
+
+    stream.stop_stream()
+    stream.close()
+    p.terminate()
+
+    wf = wave.open(WAVE_OUTPUT_FILENAME, 'wb')
+    wf.setnchannels(CHANNELS)
+    wf.setsampwidth(p.get_sample_size(FORMAT))
+    wf.setframerate(RATE)
+    wf.writeframes(b''.join(frames))
+    wf.close()
+
+    r = speech_recognition.Recognizer()
+    with speech_recognition.AudioFile(WAVE_OUTPUT_FILENAME) as source:
+        audio = r.record(source)
+        result = r.recognize_google(audio, language='ja-JP')
 
     return result
